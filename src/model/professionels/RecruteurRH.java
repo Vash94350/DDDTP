298a6c17876232dd9568df0c05d8dc810a53e286
Guilhem Torgue <torgue.guilhem@gmail.com>
2019-04-19 15:51:42 +0200

    commitMessage

--- src/model/professionels/RecruteurRH.java
+++ src/model/professionels/RecruteurRH.java
@@ -10,7 +10,7 @@ public class RecruteurRH extends Recruteur
 
     public void planifieReunion()
     {
-        Entretien nouvelEntretien = new Entretien();
+        //Entretien nouvelEntretien = new Entretien();
         //transferer des infos entre couche == creation DTO
         //Controller approche API
         //Request_entretien_DTO contient parexemple nom et date de l'entretien
